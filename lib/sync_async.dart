e4b3c7742e42a4668194ab759b79f60a65ff86af
BuildTools <unconfigured@null.spigotmc.org>
2023-11-27 11:29:22 -0300

    aula41_completed

--- lib/sync_async.dart
+++ lib/sync_async.dart
@@ -1,13 +1,50 @@
+import 'dart:convert';
+import 'dart:ui';
 import 'package:http/http.dart' as http;
 
-void main() {
-  fetch('6558a88206abe346dd67bd0b');
+Future main() async {
+  final todo = await fetch();
+  print(todo.title);
 }
 
-fetch(String id) async {
-  var url = Uri.parse('https://calm-plum-jaguar-tutu.cyclic.app/');
-  var urlGetById =
-      Uri.parse('https://calm-plum-jaguar-tutu.cyclic.app/todos/$id');
-  var response = await http.get(urlGetById);
-  print(response.body);
+Future<ToDo> fetch() async {
+  var url = Uri.parse('https://jsonplaceholder.typicode.com/todos/1');
+  var response = await http.get(url);
+  var json = jsonDecode(response.body);
+  var todo = 
+  ToDo.fromJson(json);
+  return todo;
+}
+
+
+// flutter: {
+//   "userId": 1,
+//   "id": 1,
+//   "title": "delectus aut autem",
+//   "completed": false
+// }
+class ToDo {
+  final String title;
+  final int id;
+  final int userId;
+  final bool completed;
+
+  ToDo({required this.title,required this.id,required this.userId, required this.completed});
+
+  factory ToDo.fromJson(Map json){
+    return ToDo(
+  id: json['id'],
+  title: json['title'],
+  completed: json['completed'],
+  userId: json['userId'] );
+  }
+  
+  Map toJson (){
+    return {
+      'id': id, 
+      'userId': userId, 
+      'completed': completed, 
+      'title': title, 
+    };
+  }
 }
